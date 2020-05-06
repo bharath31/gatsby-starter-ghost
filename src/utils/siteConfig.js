aaf418eafe0c389ef9833e44dea4d03d1e0d3df8
bharath31 <ln.bharath94@gmail.com>
2020-05-06 12:38:12 +0530

    added site url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://romantic-darwin-8c3c7c.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
